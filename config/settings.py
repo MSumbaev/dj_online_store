ef213bdbf5485e04a86c8aae77bc34576785d808
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-08-16 22:37:37 +0300

    changed settings.py

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dj_online_store',
+        'USER': 'postgres',
+        'PASSWORD': 'b6gFXb',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
@@ -118,6 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
