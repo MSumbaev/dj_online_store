ddcbd1ed9fb6666dbc4d8aab5e4d3117bff2b702
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-08-17 22:02:07 +0300

    changed models.py settings.py urls.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'purchase_price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('title', 'description',)
+
