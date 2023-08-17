e64fc6ee7055e35d061b3c44f1f0aa70fce573c1
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-08-17 23:42:17 +0300

    changed settings.py

--- config/settings.py
+++ config/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dj_online_store',
-        'USER': 'postgres',
-        'PASSWORD': 'b6gFXb',
+        'USER': '#',
+        'PASSWORD': '#',
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
