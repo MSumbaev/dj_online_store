591181cc4121559f3c7ef499ca436d1214c9cc61
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-08-26 23:07:14 +0300

    changed blogs/views.py, requirements.txt, blog_detail.html

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
+from pytils.translit import slugify
 
 from blogs.models import Blog
 
@@ -10,11 +11,28 @@ class BlogCreateView(CreateView):
     fields = ('title', 'body', 'preview')
     success_url = reverse_lazy('blogs:blog_list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+
+        return super().form_valid(form)
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'body', 'preview')
     success_url = reverse_lazy('blogs:blog_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blogs:blog_detail_view', args=[self.kwargs.get('pk')])
 
 
 class BlogDetailView(DetailView):
@@ -23,12 +41,23 @@ class BlogDetailView(DetailView):
         'title': 'SkyStore',
     }
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
 
 class BlogListView(ListView):
     model = Blog
     extra_context = {
         'title': 'Статьи'
     }
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
 
 
 class BlogDeleteView(DeleteView):
