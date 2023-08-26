d8011527f401edf0975c67d4ad3a6bdfb7211faa
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-08-26 19:54:23 +0300

    changed urls.py and views.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product, category
+from catalog.views import home, contacts, CategoryCreateView, CategoryListView, ProductsListView, \
+    CategoryUpdateView, CategoryDeleteView, ProductCreateView, ProductUpdateView, ProductDeleteView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('products/<int:pk>', product, name='product'),
-    path('categories/<int:pk>', category, name='category')
+    # path('products/<int:pk>', product, name='product'),
+    path('category/<int:pk>', ProductsListView.as_view(), name='category'),
+    path('category/create/', CategoryCreateView.as_view(), name='category_create'),
+    path('category/update/<int:pk>', CategoryUpdateView.as_view(), name='category_update'),
+    path('category/delete/<int:pk>', CategoryDeleteView.as_view(), name='category_delete'),
+    path('category_list/', CategoryListView.as_view(), name='category_list'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/update/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
+    path('product/view/<int:pk>', ProductDetailView.as_view(), name='product_detail_view'),
+    path('product/delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,40 +1,90 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from catalog.models import Product, Category
 
 
 def home(request):
-    product_list = Product.objects.all()
-    categories_list = Category.objects.all()
+    product_list = Product.objects.all().order_by('date_of_last_modification')
     context = {
         'object_list': product_list,
-        'categories_list': categories_list,
-        'title': 'SkyStore'
+        'title': 'Все товары'
     }
 
     return render(request, 'catalog/home.html', context)
 
 
-def category(request, pk):
-    product_list = Product.objects.filter(category_id=pk)
-    category_obj = Category.objects.get(pk=pk)
-    context = {
-        'object_list': product_list,
-        'category': category_obj,
-        'title': category_obj.title,
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = ('title', 'description',)
+    success_url = reverse_lazy('catalog:category_list')
+
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = ('title', 'description',)
+    success_url = reverse_lazy('catalog:category_list')
+
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    success_url = reverse_lazy('catalog:category_list')
+
+
+class CategoryListView(ListView):
+    model = Category
+    extra_context = {
+        'title': 'Категории товаров'
     }
 
-    return render(request, 'catalog/category.html', context)
+
+class ProductsListView(ListView):
+    model = Product
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(category_id=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+
+        category_item = Category.objects.get(pk=self.kwargs.get('pk'))
+        context_data['category_pk'] = category_item.pk
+        context_data['title'] = category_item.title
+
+        return context_data
 
 
-def product(request, pk):
-    product_obj = Product.objects.get(pk=pk)
-    context = {
-        'object': product_obj,
+class ProductDetailView(DetailView):
+    model = Product
+    extra_context = {
         'title': 'SkyStore',
     }
 
-    return render(request, 'catalog/product.html', context)
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('title', 'description', 'category', 'image', 'purchase_price')
+
+    def get_success_url(self):
+        return reverse('catalog:category', args=[self.object.category.pk])
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ('title', 'description', 'category', 'image', 'purchase_price')
+
+    def get_success_url(self):
+        return reverse('catalog:category', args=[self.object.category.pk])
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+
+    def get_success_url(self):
+        return reverse('catalog:category', args=[self.object.category.pk])
 
 
 def contacts(request):
