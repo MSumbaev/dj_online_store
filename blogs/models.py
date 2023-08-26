6f96f52c53a6959b390a5d1d1460d423ec512ab6
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-08-26 22:05:19 +0300

    added app blog

---
+++ blogs/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    slug = models.CharField(max_length=100, verbose_name='slug', **NULLABLE)
+    body = models.TextField(verbose_name='Содержимое')
+    preview = models.ImageField(upload_to='blogs/', verbose_name='Превью', **NULLABLE)
+    date_of_creation = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=True, verbose_name='Признак публикации')
+    views_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'блог'
+        verbose_name_plural = 'блоги'
---
+++ blogs/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
+
+from blogs.models import Blog
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'body', 'preview')
+    success_url = reverse_lazy('blogs:blog_list')
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'body', 'preview')
+    success_url = reverse_lazy('blogs:blog_list')
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    extra_context = {
+        'title': 'SkyStore',
+    }
+
+
+class BlogListView(ListView):
+    model = Blog
+    extra_context = {
+        'title': 'Список блогов'
+    }
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blogs:blog_list')
