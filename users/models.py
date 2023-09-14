e01b8d3005a2bc3e67a9b588671903ef7810782f
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-09-14 21:39:21 +0300

    updated users/views.py, users/models.py, users/admin.py; created users/forms.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from catalog.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+
+    phone = models.CharField(max_length=30, verbose_name='Телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    country = models.CharField(max_length=100, verbose_name='Страна', **NULLABLE)
+    verify_key = models.CharField(max_length=15, verbose_name='Ключ верификации', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
